ef892238cdd82aeefc5c49c42d1c01afebff3bf8
Vin <vlou1@nait.ca>
2025-09-10 13:45:41 -0600

    Finished buttons

---
+++ app/src/main/java/com/example/listycity/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.listycity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+	ListView cityList;
+	ArrayAdapter<String> cityAdapter;
+	ArrayList<String> dataList;
+
+	Button addCity;
+	Button deleteCity;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		cityList = findViewById(R.id.city_list);
+
+		String[] cities = {"Edmonton", "Vancouver", "Moscow", "Sydney", "Berlin", "Vienna", "Tokyo", "Beijing", "Osaka", "New Delhi"};
+
+		dataList = new ArrayList<>();
+		dataList.addAll(Arrays.asList(cities));
+
+		cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+		cityList.setAdapter(cityAdapter);
+
+	}
+
+
+}
