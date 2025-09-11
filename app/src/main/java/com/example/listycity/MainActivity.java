968f570638d25bf94742a1df760f860bd22e87ec
Vin <vlou1@nait.ca>
2025-09-11 11:51:18 -0600

    Finished Lab 2

--- app/src/main/java/com/example/listycity/MainActivity.java
+++ app/src/main/java/com/example/listycity/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.listycity;
 
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -23,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
 
 	Button addCity;
 	Button deleteCity;
+	Button confirmCity;
+	EditText inputCity;
+
+	String selectedCity;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +47,73 @@ public class MainActivity extends AppCompatActivity {
 		cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
 		cityList.setAdapter(cityAdapter);
 
+		addCity = findViewById(R.id.add_city);
+		deleteCity = findViewById(R.id.delete_city);
+		confirmCity = findViewById(R.id.confirm_add);
+		inputCity = findViewById(R.id.input_city);
+
+		cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				selectedCity = cityList.getItemAtPosition(position).toString();
+
+				for (int i = 0; i < cityList.getChildCount(); i++)
+				{
+					if (position == i)
+					{
+						cityList.getChildAt(i).setBackgroundColor(Color.LTGRAY);
+					}
+					else
+					{
+						cityList.getChildAt(i).setBackgroundColor(Color.TRANSPARENT);
+					}
+				}
+			}
+		});
+
+
+		addCity.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v)
+			{
+				inputCity.setVisibility(View.VISIBLE);
+				confirmCity.setVisibility(View.VISIBLE);
+			}
+		});
+
+		deleteCity.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v)
+			{
+				if (selectedCity == null) { return; }
+				dataList.remove(selectedCity);
+				cityAdapter.notifyDataSetChanged();
+
+				for (int i = 0; i < cityList.getChildCount(); i++)
+				{
+					cityList.getChildAt(i).setBackgroundColor(Color.TRANSPARENT);
+				}
+			}
+		});
+
+		confirmCity.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v)
+			{
+				String input = inputCity.getText().toString().strip();
+
+				if (input.equals("")){ return; }
+				dataList.add(input);
+				cityAdapter.notifyDataSetChanged();
+
+				inputCity.setText("");
+
+				inputCity.setVisibility(View.INVISIBLE);
+				confirmCity.setVisibility(View.INVISIBLE);
+			}
+		});
 	}
+
 
 
 }
